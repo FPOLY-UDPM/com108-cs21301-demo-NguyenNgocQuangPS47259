09a7041241b0fffda366268f252779a854326b04
Tên của bạn <quangnguyendeptrai1711@gmail.com>
2026-01-10 08:40:14 +0700

    nộp lap 1

---
+++ lap1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    //giới thiệu phần mềm
+    printf("Hello, World!\n");
+    printf("ten toi la Nguyen Ngoc Quang PS47259\n");
+
+    //nhập tổng
+    int x =0,y=0,z;
+    printf("xin hãy nhập giá trị cho X :");
+    scanf("%d",&x);
+    printf("xin hãy nhập giá trị cho y :");
+    scanf("%d",&y);
+    z= x+y;
+    printf("vậy tổng của X và y là : %d",z);
+return 0;
+}
